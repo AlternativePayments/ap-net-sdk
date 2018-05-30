bf638c89a195604bbc6de0c9dfee26db7aee49be
Nikola Koprivica <n.koprivica@wolf-wolf.net>
2018-05-30 15:21:03 +0200

    GetAllSepa is added

---
+++ src/AlternativePayments/Enums/TransactionStatus.cs
@@ -0,0 +1,19 @@
+﻿namespace AlternativePayments
+{
+    public enum TransactionStatus
+    {
+        Pending = 1,
+        Approved = 2,
+        Funded = 3,
+        Declined = 4,
+        Voided = 5,
+        VoidDeclined = 6,
+        Refunded = 8,
+        RefundDeclined = 9,
+        Chargeback = 10,
+        Isf = 11,
+        Invalid = 12,
+        AcquirerDown = 13,
+        Aborted = 14
+    }
+}
---
+++ src/AlternativePayments/Filters/SepaFilter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AlternativePayments
+{
+    public class SepaFilter : BaseFilter
+    {
+        public SepaFilter()
+        {
+        }
+
+        public SepaFilter WithOffset(int offset)
+        {
+            Offset = offset;
+            return this;
+        }
+
+        public SepaFilter WithLimit(int limit)
+        {
+            Limit = limit;
+            return this;
+        }
+
+        public SepaFilter WithTransactionStatus(TransactionStatus transactionStatus)
+        {
+            TransactionStatus = transactionStatus;
+            return this;
+        }
+
+        public SepaFilter WithCreatedFrom(DateTime createdFrom)
+        {
+            CreatedFrom = createdFrom;
+            return this;
+        }
+
+        public SepaFilter WithCreatedTo(DateTime createdTo)
+        {
+            CreatedTo = createdTo;
+            return this;
+        }
+
+        public SepaFilter WithUpdatedFrom(DateTime updatedFrom)
+        {
+            UpdatedFrom = updatedFrom;
+            return this;
+        }
+
+        public SepaFilter WithUpdatedTo(DateTime updatedTo)
+        {
+            UpdatedTo = updatedTo;
+            return this;
+        }
+
+        public int? Offset { get; set; }
+        public int? Limit { get; set; }
+        public TransactionStatus? TransactionStatus { get; set; }
+        public DateTime? CreatedFrom { get; set; }
+        public DateTime? CreatedTo { get; set; }
+        public DateTime? UpdatedFrom { get; set; }
+        public DateTime? UpdatedTo { get; set; }
+    }
+}
